6377bab5a847c970d23c0ea3408f25b885ec0ffa
rishu2024 <134621791+rishu2024@users.noreply.github.com>
2025-10-12 14:15:46 +0000

    conversion in datatypes

--- 01_basics/03_conversationoperation.js
+++ 01_basics/03_conversationoperation.js
@@ -48,6 +48,12 @@ let str1 = "hello"
 let str2 = "abhinav"
 let str3 = str1 + str2;
 console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(2 + 2 + "1");
+
+
 
 
 
