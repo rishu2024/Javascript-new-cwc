a3abf0c00ead1ca459eb570aa0a257463749bc16
rishu2024 <134621791+rishu2024@users.noreply.github.com>
2025-10-12 08:01:50 +0000

    updated datatypes.js file

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Abhinav")
 
 
-let name = "hitesh"
+let name = "Abhinav"
 let age = 18
 let isLoggedIn = false
 let state;
