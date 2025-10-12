344f3a0a91bf20f2ffac25d91a554fd8495b49f1
rishu2024 <134621791+rishu2024@users.noreply.github.com>
2025-10-12 13:35:09 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversationoperation.js
@@ -0,0 +1,35 @@
+// 
+
+
+// request.body
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+// let isLogggedIn = 0
+
+// let booleanisLoggedIn = Boolean(isLogggedIn);
+// console.log(booleanisLoggedIn);
+
+// 1 => true;
+// 0 => false;
+// "abhinav" => true; 
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
