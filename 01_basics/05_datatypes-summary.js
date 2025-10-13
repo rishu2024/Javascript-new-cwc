b16b2d3037d87884951617227da5f6a05e0b1573
rishu2024 <134621791+rishu2024@users.noreply.github.com>
2025-10-13 12:35:41 +0000

    datatypes-summary

--- 01_basics/05_datatypes-summary.js
+++ 01_basics/05_datatypes-summary.js
@@ -21,7 +21,7 @@ const outsideTemp = null
 const id = Symbol("123")
 const anotherid = Symbol("123")
 
-console.log( id === anotherid);
+console.log(id === anotherid);
 
 // const bignumber = 232443242n;
 // console.log();
@@ -42,16 +42,16 @@ const heros = ["shaktiman,spiderman,batman"]
 
 // 2. objects
 
- let myobj = { 
-   name: "abhinav",
-   age:  22,
+let myobj = {
+  name: "abhinav",
+  age: 22,
 };
 // object comes under {} always;
 // we also store object in variable
 
 // 3. function
 
-const myFunction = function(){
+const myFunction = function () {
   console.log("hello world");
 }
 
@@ -60,6 +60,36 @@ console.log(typeof heros);
 // every non premative data return type is only object and only in function non premative return function object
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ++++++++++++++++ memory types +++++++++++++
+// 1.stack - it is use in primative datatypes(jo bhi variable declare krte ha uska copy milta ha
+
+// 2.heap - it is use in non primative datatypes(jo bhi variable declare krte ha uska reference milta hai)
+
+let myyoutubename = "abhinav kartik"
+let anothername = myyoutubename
+anothername = "chaiwithcode"
+
+console.log(myyoutubename);
+console.log(anothername);
+
+let userOne = {
+  email: "rishu349394@gmail.com",
+  upi: "abhi4943e458@oksbi",
+}
+
+let usertwo = userOne
+
+usertwo.email  = "abhi434@gmail.com"
+
+console.log(usertwo.email);
+console.log(usertwo.email);
+
+
+
+// let student = ["snkik","abhinav","raj" ]
+// console.log(typeof(student)); its output is object
+
 
 
 
