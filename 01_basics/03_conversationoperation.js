7820fbe95b58cb5d3a4a1f4b7e8a4eb459e33ae2
rishu2024 <134621791+rishu2024@users.noreply.github.com>
2025-10-13 05:43:22 +0000

    Conversation operation datatypes

--- 01_basics/03_conversationoperation.js
+++ 01_basics/03_conversationoperation.js
@@ -44,14 +44,32 @@
 // console.log(2/2);
 // console.log(2%2);
 
-let str1 = "hello"
-let str2 = "abhinav"
-let str3 = str1 + str2;
-console.log(str3);
-console.log("1" + 2);
-console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(2 + 2 + "1");
+// let str1 = "hello"
+// let str2 = "abhinav"
+// let str3 = str1 + str2;
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1");
+
+// console.log((3 + 4) *5 % 3); not reccomended
+
+// console.log(+true); possible
+// console.log(true+); notpossible
+// console.log(+"");
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// link to study => https://tc39.es/ecma262/#sec-abstract-operations
+
+
+
 
 
 
