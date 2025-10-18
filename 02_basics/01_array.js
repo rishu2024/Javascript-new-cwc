e22e6328564774b07c1c590fd7553d4e4101c2c0
rishu2024 <134621791+rishu2024@users.noreply.github.com>
2025-10-18 07:38:40 +0000

    Array in javascript

---
+++ 02_basics/01_array.js
@@ -0,0 +1,59 @@
+// Array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myheros = ["shaktiman,nagara"]
+const arr2 = new Array(1, 2, 3, 4,)
+// console.log(myArr[1]);
+
+
+// array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// // give output all value 0 to 7
+// myArr.pop()
+// pop remove laat value from output
+// myArr.unshift(5)  
+// content inside the bracket() called argument
+// .unshift value jo bracket ke ander ha usse starting me le ata ha
+// myArr.shift()
+// .shift first number array ka remove kar deta ha
+// console.log(myArr);
+// console.log(myArr.includes(3));
+// // yeh check krta ha ki argument me jo number diye ha woo array me ha yeh nahi-true or false me output deta ha
+// console.log(myArr.indexOf(4));
+// yeh check krta ha aur btata ha woo no kis position(index) pe ha array me
+
+
+// const newarr = myArr.join()
+// // .join convert array into string
+
+// console.log(myArr);
+// console.log(newarr);
+// console.log(typeof newarr);
+
+
+// slice and splice methods- NHI SMJH AYA
+
+console.log("A", myArr);
+
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+//  this will print 1,2 not 3 last no print nahi hota hai
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("C", myArr);
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+
