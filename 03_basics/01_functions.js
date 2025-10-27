b9c1e75747a08f9cda04d4009fc151d72f43729b
rishu2024 <134621791+rishu2024@users.noreply.github.com>
2025-10-27 21:31:54 +0000

    Function in javascript

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,47 @@
+
+function sayMyName(){
+console.log("a");
+console.log("b");
+console.log("h");
+console.log("i");
+console.log("n");
+console.log("a");
+console.log("v");
+}
+// collection of code jiska hum ek package bna de usko hi function bolte ha
+
+// sayMyName()
+function addTwoNumbers(number1,number2){
+    return number1 + number2
+//    console.log( number1 + number2);
+// let result = number1 + number2
+// console.log("abhinav");
+
+return result
+console.log();
+}
+// ek baar hum function me return kar diye uske baad koi kaam nahi krege function
+addTwoNumbers()
+addTwoNumbers(3,4)
+// yeha pe number1 , number2 parameter ha aur 3,4 arguments ha
+
+const result = addTwoNumbers(3,5)
+// console.log("result:",result);
+
+function loginUserMessage(username = "abhi"){
+    if (username=== undefined) 
+        // (!username)
+        {
+        console.log("please enter a username");
+        return
+    }
+    return `${username} just logged in `
+}
+console.log(
+loginUserMessage( "abhinav"));
+console.log(
+loginUserMessage("abhinav"));
+
+
+
+
